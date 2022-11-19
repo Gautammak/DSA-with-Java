03e191cfee8f9052170cd386727fad89fbd79ec3
Gautammak <gautammakavana2001@gmail.com>
2022-11-19 11:19:26 +0530

    first commit

--- src/arrays/arrayssort.java
+++ src/arrays/arrayssort.java
@@ -1,7 +1,15 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class arrayssort {
     public static void main(String[] args) {
+        int[] a={5,6,8,1,2,9};
+        System.out.println(Arrays.toString(sort(a)));
+    }
 
+    public static int[] sort(int[] a){
+        Arrays.sort(a);
+        return a;
     }
 }
