8e0cdfced6d3659bb209bfea52d662720d73d1fd
Gautammak <gautammakavana2001@gmail.com>
2022-11-19 11:14:11 +0530

    Initial commit

---
+++ src/arrays/arrayssort.java
@@ -0,0 +1,7 @@
+package arrays;
+
+public class arrayssort {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] args) {
+        System.out.println(sum(5,9));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
